07c45702058e13bd978867537272f09237097ef6
matheusnsilva7 <silva.matheus6081@gmail.com>
2022-10-24 15:57:48 -0300

    fixing things

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,16 @@
+import Head from "next/head";
+
 import classes from "../styles/notFound.module.css";
 
 const Custom404 = () => {
-  return <h1 className={classes.tittle}>404 - Page Not Found</h1>;
+  return (
+    <>
+      <Head>
+        <title>404 Page</title>
+      </Head>
+      <h1 className={classes.tittle}>404 - Page Not Found</h1>
+    </>
+  );
 };
 
 export default Custom404;
