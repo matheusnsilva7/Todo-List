e72ba3c8649b1cb539fd27313dc1e71179636749
matheusnsilva7 <silva.matheus6081@gmail.com>
2022-12-15 09:15:20 -0300

    dom

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,20 @@
+import { useState , useEffect } from "react";
 import Navbar from "../components/Nav/Navbar";
 import { StoreContextProvider } from "../components/Store/store-context";
 
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  const [domLoaded, setDomLoaded] = useState(false)
+
+  useEffect(() => {
+    setDomLoaded(true)
+  }, [])
+
+  if (!domLoaded) {
+    return null
+  }
+
   return (
     <StoreContextProvider>
       <Navbar />
