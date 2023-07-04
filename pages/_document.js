df92af54ac6c242d19ea650dcc00d24b90da5b9a
matheusnsilva7 <silva.matheus6081@gmail.com>
2023-07-04 20:58:29 -0300

    new design

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head />
-      <body style={{ backgroundImage: "url('/background2.jpg')" }} >
+      <body style={{ backgroundImage: "url('/background.jpg')" }} >
         <Main />
         <NextScript />
       </body>
